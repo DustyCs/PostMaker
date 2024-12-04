3d17303e039245479ed4de784ed788042d56a968
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-12-04 16:14:38 +0800

    Added some designs and fixed the layout

--- index.php
+++ index.php
@@ -3,42 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/static/css/master.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="/static/js/jquery-3.7.1.min.js"></script>
+    <script src="/static/js/siteEffects.js"></script>
     <title>Document</title>
 </head>
 <body>
-   <div class="previewer">
-        <div class="main-preview">
+    <div class="container">
+        <div class="main-previewer">
+            <div class="preview">
+                em
+            </div>
+        </div>
+        <div class="interface">
+            <div class="file-interface">
+                <form action="" class="file-form">
+                    <div class="file-selection file-input">
+                        <label for="select-file" id="select-input">SELECT FILE</label>
+                        <input type="file" id="select-file">
+                    </div>
+                    <div class="file-saver file-input">
+                        SAVE FILE
+                    </div>
+                </form>
+            </div>
+            <div class="main-interface">
+                <div class="interface-forms">
+                    <form action="" class="main-form">
+                        <div class="title">
+                            <label for="title">Add Title</label>
+                            <input type="text" name="" id="title">
+                            <input type="submit" value="">
+                        </div>
+                        <div class="description">
+                            <label for="description">Add Description</label>
+                            <input type="text" name="" id="description">
+                            <input type="submit" value="">
+                        </div>
+                        <div class="image">
+                            <label for="image">Add Image</label>
+                            <input type="file" name="" id="image">
+                        </div>
+                        <div class="links">
+                            <label for="links">Add Links</label>
+                            <input type="text" name="" id="links">
+                        </div>
+                    </form>
+                </div>
+                <div class="file-backup">
 
-        </div>
-   </div>
-   <div class="interface">
-    <div class="file-interface">
-        <form action="">
-            <label for="open-file">Select File</label>
-            <input type="file" name="" id="">
-            <label for="save-file">Save File</label>
-            <input type="file" name="" id="">
-        </form>
-    </div>
-    <div class="main-interface">
-        <div class="interface-forms">
-            <form action="">
-                <label for="title">Add Title</label>
-                <input type="text" name="" id="">
-                <label for="description">Add Description</label>
-                <input type="text" name="" id="">
-                <label for="image">Add Image</label>
-                <input type="file" name="" id="">
-                <label for="links">Add Links</label>
-                <input type="text" name="" id="">
-            </form>
-        </div>
-        <div class="file-backup">
-
+                </div>
+            </div>
         </div>
     </div>
-   </div>
 </body>
 </html>
