b14f91d055867defc202fb3b996a67b2081051b5
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-12-04 14:43:58 +0800

    Files Added

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="/static/js/jquery-3.7.1.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+   <div class="previewer">
+        <div class="main-preview">
+
+        </div>
+   </div>
+   <div class="interface">
+    <div class="file-interface">
+        <form action="">
+            <label for="open-file">Select File</label>
+            <input type="file" name="" id="">
+            <label for="save-file">Save File</label>
+            <input type="file" name="" id="">
+        </form>
+    </div>
+    <div class="main-interface">
+        <div class="interface-forms">
+            <form action="">
+                <label for="title">Add Title</label>
+                <input type="text" name="" id="">
+                <label for="description">Add Description</label>
+                <input type="text" name="" id="">
+                <label for="image">Add Image</label>
+                <input type="file" name="" id="">
+                <label for="links">Add Links</label>
+                <input type="text" name="" id="">
+            </form>
+        </div>
+        <div class="file-backup">
+
+        </div>
+    </div>
+   </div>
+</body>
+</html>
