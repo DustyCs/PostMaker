75a367de8343158f9f523ebf4e6e5280b234d384
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-12-09 15:22:37 +0800

    php-dev - finally able to write file from website

--- includes/jsonData.includes.php
+++ includes/jsonData.includes.php
@@ -3,15 +3,27 @@
 require "../classes/jsonDataContr.php";
 
 $type = $_POST['type'];
-$dataContr = new JsonDataContr($file, $file_location); 
+$dataContr = new JsonDataContr; 
 
 if ($type == "select"){
     $file = $_POST['filename'];
+    $file_location = "/../output/";
+
+    $dataContr->file = $file;
+    $dataContr->file_location = $file_location;
 }
     
 if ($type == "run"){
     $json = filter_input(INPUT_POST, 'json');
     $decoded = json_decode($json);
+    $save_location = "/../output/";
+    $file_name = "test";
+
+    $dataContr->data = $decoded; // No need to run setData since this is already the data
+    $dataContr->editData();
+    // Set Data URL BEFORE RUNNING RENDER DATA
+    $dataContr->data_url = __DIR__ . $save_location . $file_name . ".json";
+    $dataContr->renderData(); 
 
     ECHO var_dump($decoded) . var_dump($type);
 
@@ -20,14 +32,23 @@ if ($type == "run"){
 if ($type == "save"){
     
 }
-// $file_location = "/../output/";
-// $dataContr = new JsonDataContr($file, $file_location); // $file is only sent if type is select in case of run the json itself should be sent
+
+if ($type == "backup"){
+    
+}
+
 // $dataContr->editData();
 // $dataContr->renderData();
 
 // echo $dataContr->data[0];
 // ECHO var_dump($object);
 
+
+
+// to do 
+// have the data from the form be written into the json file
+// read the json file data if there is "properly readable data"
+// if none then show something else
 
 
 
